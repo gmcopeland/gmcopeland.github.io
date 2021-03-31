f1bf8f1b437c96ca3f40f174e906fba80f4e8733
Garrett Copeland <gmcopeland@mavs.coloradomesa.edu>
2021-03-31 15:51:31 -0600

    after class

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Garrett Copelands Git Portfolio </h1>
       </header>
+      <body>
+        <h1> Welcome to my Portfolio</h1>
+        <h2> Thank you for visiting .</h2>
+      </body>
+        <p> I am a graduating Computer Science Student with a focus on Cyber Security!
+          This portfolio will contain both individual and academic projects I have worked on
+          the last couple years. Some may be complete, some may be starters that I haven't had the time
+          to finish. Regardless, please enjoy your stay and check back often!
+        </p>
+        <div class = "menu">
+          <ul>
+            <li><a href="">See My projects!</a></li>
+            <li><a href="">Register as a visitor!</a></li>
+            <li><a href=""> View more about me!</a></li>
+          </ul>
+        </div>
+
     </div>
+    
+
   );
 }
 
